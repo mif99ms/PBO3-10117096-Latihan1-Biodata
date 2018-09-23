2733a1af7e65e1bba5f001e107559d6a93f84813
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:30:08 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan1/biodata/PBO310117096Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan biodata
+ */
+public class PBO310117096Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Miftah Muhammad Sidqi");
+        System.out.println("ALAMAT\t\t: Kanayakan Timur Dago");
+        System.out.println("TEMPAT LAHIR\t: Garut");
+        System.out.println("TANGGAL LAHIR\t: 02 Mei 1999");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Main Game, Internetan, Nonton Youtube");
+        System.out.println("MAKES\t\t: Martabak Telor & Ayam Geprek");
+        System.out.println("MIKES\t\t: Jus Jambu");
+        
+    }
+    
+}
